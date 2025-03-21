3355aef60656661cb8ea4fb1415e2c19aca410e3
Shravan Samudrala <shravans5811@gmail.com>
2025-03-21 13:27:31 -0500

    Code added

--- common-string-methods.js
+++ common-string-methods.js
@@ -69,23 +69,46 @@ Complete the following tasks and assign the results to the specified variables. 
 let inputString = "  Welcome to the Coding Bootcamp! Learn JavaScript today.  ";
 
 // 1. Searching
-let hasJavaScript; // Your code here
+let hasJavaScript; // Your code here 
+let hasJavaScript = inputString.includes("JavaScript");
+
 let codingPosition; // Your code here
+let codingPosition = inputString.indexOf("Coding");
+
 let startsWithWelcome; // Your code here
+let startsWithWelcome = inputString.trim().startsWith("Welcome");
+
 let endsWithToday; // Your code here
+let endsWithToday = inputString.trim().endsWith("today.");
+
 
 // 2. Transforming
 let lowercaseString; // Your code here
+let lowercaseString = inputString.toLowerCase();
+
 let uppercaseString; // Your code here
+let uppercaseString = inputString.toUpperCase();
+
 let trimmedString; // Your code here
+let trimmedString = inputString.trim();
+
 let replacedString; // Your code here
+let replacedString = inputString.replace("JavaScript","coding");
 
 // 3. Breaking Apart
 let wordsArray; // Your code here
+let wordsArray = trimmedString.split(" ");
+
 
 // 4. Retrieving
 let firstCharacter; // Your code here
+let firstCharacter = trimmedString.charAt(0);
+
 let extractedBootcamp; // Your code here
+let bootcampIndex = trimmedString.indexOf("Bootcamp");
+let extractedBootcamp = trimmedString.slice(bootcampIndex, bootcampIndex + "Bootcamp".length);
+
+
 
 // Log all results
 console.log({
